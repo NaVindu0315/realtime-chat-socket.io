1ebbc8c31280e70e18adfb3747ff25d89d6d4e28
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-28 21:56:02 +0530

    Add chat room join functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,26 @@ import io from "socket.io-client";
 
 const socket = io.connect("http://localhost:3001");
 function App() {
+
+  const [username,setuserrname] = useState('');
+  const [room,setroom] = useState(''); 
+
+  const joinRoom = () => { 
+
+   }
   return (
     <div className="App">
-     dfsf
+     <h3>Join a chat</h3>
+
+     <input type="text" placeholder="Ponna Balla" 
+     onchange= {(event)=>
+     
+     {
+      setuserrname(event.target.value);
+     }
+     } />
+     <input type="text" placeholder="Room ID" />
+     <button>Join a Room</button>
     </div>
   );
 }
